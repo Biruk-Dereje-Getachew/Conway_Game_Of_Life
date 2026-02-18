7cca65e779b35765089cf6654410e6b831e8bebb
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-02-18 15:39:00 +0300

    Changed lit to alive and added location attribute

--- Cell.py
+++ Cell.py
@@ -1,7 +1,8 @@
 class Cell:
-    lit = False
+    alive = False
+    location = [0, 0]
     neighbors = []
-    def __init__(self, lit=False, neighbors=[]):
-        self.lit = lit
+    def __init__(self, alive=False, location = [0, 0], neighbors=[]):
+        self.alive = alive
+        self.location = location
         self.neighbors = neighbors
-        
