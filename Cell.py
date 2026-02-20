0e3fa3face65d1b87ad5d9772d607ccf45be780a
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-02-20 23:49:06 +0300

    Used int instead of bool for alive status of cells

--- Cell.py
+++ Cell.py
@@ -1,10 +1,10 @@
 class Cell:
-    alive = False
-    next_state = False
+    alive = 0
+    next_state = 0
     location = [0, 0]
     neighbors = []
 
-    def __init__(self, alive=False, location = [0, 0], neighbors=[]):
+    def __init__(self, alive=0, location = [0, 0], neighbors=[]):
         self.alive = alive
         self.next_state = alive
         self.location = location
@@ -13,14 +13,14 @@ class Cell:
     def determine_state(cell):
         alive_count = 0
         for neighbor in cell.neighbors:
-            if neighbor.alive == True: alive_count += 1
+            if neighbor.alive == 1: alive_count += 1
         
         if cell.alive:
-            if alive_count < 2: cell.next_state = False
-            elif alive_count >= 4: cell.next_state = False
-            else: cell.next_state = True
+            if alive_count < 2: cell.next_state = 0
+            elif alive_count >= 4: cell.next_state = 0
+            else: cell.next_state = 1
         else:
-            if alive_count == 3: cell.next_state = True
+            if alive_count == 3: cell.next_state = 1
     
     def __str__(self):
         return f"{self.alive}" + str(self.location)
