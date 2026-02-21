88c93a7576e3f5478c9404345acc024cf5c44363
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-02-21 18:31:35 +0300

    Rewrote the hydrate function to use a simple nested for loop method

--- Canvas.py
+++ Canvas.py
@@ -10,40 +10,13 @@ class Canvas:
         self.hydrate(random)
 
     def hydrate(self, random):
-        alive = 0 if not random else randint(0, 1)
-        initial_cell = Cell(alive, [0, 0])
-        cells_to_neighborate = [initial_cell] # Initial cell added
-        self.grid.append([initial_cell])
-        track_neighbors = {(initial_cell.location[0], initial_cell.location[1])}
-
-        for cell in cells_to_neighborate:
-            location = cell.location
-
-            for angle in range(180, -180, -45):
-                rad_angle = radians(angle)
-                cos_angle = round(cos(rad_angle), 2)
-                sin_angle = round(sin(rad_angle), 2)
-                offset_x = int(cos_angle / abs(cos_angle)) if cos_angle != 0 else 0
-                offset_y = int(sin_angle / abs(sin_angle)) if sin_angle != 0 else 0
-
-                nei_x = location[0] + offset_x
-                nei_y = location[1] + offset_y
-
-                if (nei_x < 0 or nei_x >= self.size[0] or nei_y < 0 or nei_y >= self.size[1]) or ((nei_x, nei_y) in track_neighbors):
-                    continue
-
-                nei_location = [nei_x, nei_y]
-                nei_alive = randint(0, 1) if random else 0
-
-                neighbor = Cell(nei_alive, nei_location)
-                cells_to_neighborate.append(neighbor)
-                track_neighbors.add((nei_x, nei_y))
-
-                if len(self.grid) <= nei_x: self.grid.insert(nei_x, [])
-                if nei_y >= len(self.grid[nei_x]):
-                    for _ in range((nei_y + 1) - len(self.grid[nei_x])):
-                        self.grid[nei_x].append(object())
-                self.grid[nei_x][nei_y] = neighbor
+        for x in range(self.size[0]):
+            row = []
+            for y in range(self.size[1]):
+                cell_alive = randint(0, 1) if random else 0
+                cell = Cell(cell_alive, [x, y])
+                row.append(cell)
+            self.grid.append(row)
 
     def __str__(self):
         canvas_description = ""
