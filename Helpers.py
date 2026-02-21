f2ec953d133c7f977d5345f31206f31715b14a01
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-02-21 19:50:47 +0300

    Removed the class and made it a simple module

--- Helpers.py
+++ Helpers.py
@@ -1,16 +1,11 @@
-class Helpers:
+def rectangle(self, turtle, width, height, location=[], heading=0):
+    turtle.penup()
+    turtle.goto(location) if location else 0
+    turtle.pendown()
+    turtle.setheading(heading)
 
-    def __init__(self):
-        pass
-
-    def rectangle(self, turtle, width, height, location=[], heading=0):
-        turtle.penup()
-        turtle.goto(location) if location else 0
-        turtle.pendown()
-        turtle.setheading(heading)
-
-        for _ in range(2):
-            turtle.forward(width)
-            turtle.right(45)
-            turtle.forward(height)
-            turtle.right(45)
+    for _ in range(2):
+        turtle.forward(width)
+        turtle.right(45)
+        turtle.forward(height)
+        turtle.right(45)
