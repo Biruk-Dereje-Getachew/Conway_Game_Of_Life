c2d76858a115a226db869cd1011abbba59a3ace7
Biruk-Dereje-Getachew <biruk.scorpio.dereje@gmail.com>
2026-02-21 20:26:38 +0300

    Changed the rectangle function to square function

--- Helpers.py
+++ Helpers.py
@@ -1,11 +1,12 @@
-def rectangle(self, turtle, width, height, location=[], heading=0):
+def square(turtle, color, width, location=[], heading=0):
+    turtle.fillcolor(color)
     turtle.penup()
-    turtle.goto(location) if location else 0
+    turtle.goto(location[0] * width, location[1] * width) if location else 0
     turtle.pendown()
     turtle.setheading(heading)
 
-    for _ in range(2):
+    turtle.begin_fill()
+    for _ in range(4):
         turtle.forward(width)
-        turtle.right(45)
-        turtle.forward(height)
-        turtle.right(45)
+        turtle.right(90)
+    turtle.end_fill()
